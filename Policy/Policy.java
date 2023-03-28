b0ed7a091d3e43571bc6eba798c2d81d17f734f0
Dmaz12 <100397746+Dmaz12@users.noreply.github.com>
2023-03-27 23:32:30 -0400

    Add files via upload

--- Policy/Policy.java
+++ Policy/Policy.java
@@ -1,180 +1,184 @@
-import java.util.*;
-public class Policy
-{  
-
-
-
-   final double BASE_PRICE = 600;// final vlaue for bse price 
-   final double AGE_FEE = 50; // final value for age fee 
-   final double SMOKE_FEE = 100; // final value for smoker fee 
-   /**
-   Below****
-   Creates the private feilds for the class needed 
+public class Policy{
+  
    
-   */
-
-   private int number;
-   private String provider;
-   private String nameF;
-   private String nameL;
-   private int age;
-   private String smoke;
-   private double height;
-   private double weight;
-   private double BMI;
-   private double addFee;
+      private int number; // user number 
+      private String provider;  // user provider 
+      private String fName; // user first name 
+      private String lName; // user last name 
+      private int age;  // user age 
+      private String smoke; //user smoke status 
+      private double height; // user height 
+      private double weight; // user weight 
    
-   private double policyPrice = 0;
-   
-   // construcot that takes no argument 
-   public Policy()
-   {
-      number = 00;
+ 
+   // no args constructor 
+   public Policy(){
+      
+      number = 0;
       provider = "";
-      nameF = "";
-      nameL = "";
-      age = 18;
+      fName = "";
+      lName = "";
+      age = 0;
       smoke = "";
       height = 0;
-      weight = 0; 
-      
-   }
-   // constructor that takes areguments 
-   public Policy(int numberIn, String prov, String nameFIn, String nameLIn, int ageIn,String smokeIn, double heightIn, double weightIn)
-   {
-      number = numberIn;
-      provider = prov;
-      nameF = nameFIn;
-      nameL = nameLIn;
-      age = ageIn;
-      smoke = smokeIn;
-      height = heightIn;
-      weight = weightIn; 
+      weight = 0;
       
    }
    
-   // setter for the policy number 
-   public void setNumber(int numberIn)
-   {
-      number = numberIn;
+   // construcotr that takes argument 
+   public Policy(int pNumber, String pProvider, String first, String last, int pAge, String inSmoke, double pHeight, double pWeight){
+   
+      number = pNumber;
+      provider = pProvider;
+      fName = first;
+      lName = last;
+      age = pAge;
+      smoke = inSmoke;
+      height = pHeight;
+      weight = pWeight;
+      
+   
    }
-   // getter for the policy number 
-   public int getNumber()
-   {
+   
+   // getter and setter for all attributtes 
+   public void setNumber(int pNumber){
+      number = pNumber;
+   }
+   
+   public int getNumber(){
+      
       return number;
    }
    
-   // setter for the provider 
-   public void setProvider(String prov)
-   {
-      provider = prov;
+   public void setProvider(String pProvider){
+      
+      provider = pProvider;
    }
    
-   // getter for the policy proivider 
-   public String getProvider()
-   {
-      return provider; 
+   public String getProvider(){
+   
+      return provider;
    }
    
-   // setter for policy holders first name 
-   public void setNameF(String nameFIn)
-   {
-      nameF = nameFIn;
-   }
-   // getter for policy holders first name
-   public String getNameF()
-   {
-      return nameF;
+   public String getFirst(){
+      
+      return fName;
    }
    
-   // setter for policy holders Last name
-   public void setNameL(String nameLIn)
-   {
-      nameL = nameLIn;
+   public void setFirst(String first){
+      
+      fName = first; 
    }
    
-   //getter for policy holders Last name
-   public String getNameL()
-   {
-      return nameL;
-   }
-   // setter for policy holders age
-   public void  setAge(int ageIn)
-   {
-      age = ageIn;
+   public String getLast(){
+      
+      return fName; 
    }
    
-   //getter for policy holders age
-   public int getAge()
-   {
+   public void setLast(String last){
+   
+      lName = last;
+   }
+   
+   public void setAge(int pAge){
+   
+      age = pAge;
+   }
+   
+   public int getAge(){
+   
       return age;
    }
-   // setter for policy holders smoking status 
-   public void setSmoke(String smokeIn)
-   {
-      smoke = smokeIn;
+   
+   public void setSmokeStatus(String inSmoke){
+   
+      smoke = inSmoke;
    }
    
-   // getter for policy holders smoking status
-   public String getSmoke()
-   {
+   public String getSmokeStatus(){
+      
       return smoke;
    }
-   public void setHeight(double heightIn)
-   {
-      height = heightIn;
-   }
    
-   public double getHeight()
-   {
-      return height;
-   }
-   
-   public void setWeight(double weightIn)
-   {
-      weight = weightIn;
-   }
-   
-   public double getWeight()
-   {
-      return weight;
-   }
-   
-   // ,ethod that calcualtes the BMI fomr the users input 
-   public double getBMI()
-   {
-   return BMI =  (weight * 703 ) / (height*height );
-   }
-   
-   // method that retruns the additonal fee of your BMI 
-   public double getAddFee()
-   {
-      return (BMI - 35)*20;
-   }
-   
-   /** 
-   Policy price method 
-   it takes the information from the user and outputs the insurane policy price from the 3 major factors (agr,BMI, smoking status)
-   
-   */
-   public double getPolicyPrice()
-   {
-      policyPrice = BASE_PRICE; 
-     
-        if(BMI >35)
-        {
-         policyPrice+= getAddFee();
-        }
-        if(age > 50)
-        {
-         policyPrice+= AGE_FEE;
-        }
-        if(smoke.toLowerCase() == "smoker")
-        {
-         policyPrice += SMOKE_FEE;
-        }
-            
+   public void setHeight(double pHeight){
       
-         return policyPrice;
+      height = pHeight;
    }
+   
+   public double getHeight(){
+   
+      return height;
+   
+   }
+   
+   public void setWeight(double pWeight){
+      
+      weight = pWeight;
+   }
+   
+   public double getWeight(){
+   
+      return weight;
+   
+   }
+  
+      //BMI method
+   public double getBMI(){
+   
+      final double con = 703;
+   
+     return (weight * con)/(height*height);
+      
+   
+   }
+    
+   
+      // policy price method 
+   public double getPrice(){
+   
+   final double BASE_PRICE = 600;
+   final double AGE_FEE = 75; 
+   final double SMOKING_FEE = 100;
+   final double FEE_PER_BMI = 20;
+   
+   final int AGE_THRESH = 50;
+   final int BMI_THRESH = 35;
+   
+    double price = BASE_PRICE;
+   
+   if (age > AGE_THRESH)
+      price += AGE_FEE;
+      
+      if (smoke.equalsIgnoreCase("smoker"))
+         price += SMOKING_FEE;
+         
+         if(getBMI() > BMI_THRESH)
+            price += ((getBMI()  - BMI_THRESH )* FEE_PER_BMI);
+         
+      
+   return price;
+   
+   
+   
+   
+      
+      
+   }
+  
+  
+  public String toString()// csll to Striong method 
+   {
+      System.out.println("Policy Number: " + number);
+      System.out.println("Provider Name: " + provider);
+      System.out.println("Policyholder's First Name: " + fName);
+      System.out.println("Policyholder's Last Name: " + lName);
+      System.out.println("Policyholder's Age: " + age);
+      System.out.println("Policyholder's Smoking Status (Y/N): " + smoke);
+      System.out.println("Policyholder's Height: " + height + " inches");
+      System.out.println("Policyholder's Weight: " + weight + " pounds");
+      System.out.printf("Policyholder's BMI: %.2f\n", getBMI());
+      System.out.printf("Policy Price: $%.2f\n", getPrice());
+   }
+
+     
 }
--- Policy/PolicyDemo.java
+++ Policy/PolicyDemo.java
@@ -1,107 +1,94 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 
 
+// policy demo class 
 public class PolicyDemo
 {
-   public static void main(String [] args)// main argument 
+
+  // main argument 
+   public static void main (String [] args)throws IOException
    {
-      Scanner input = new Scanner(System.in);// creates scanner object 
-      
-      //takes input for the policy number 
-      System.out.print("Please enter the Policy Number: "); 
-      int number = input.nextInt();
-      while (number<=0)
-      {
-         System.out.println("Please enter a valid number");
-         System.out.print("What is the policy number: "); 
-         number = input.nextInt();
-      
-      }
-      
-      input.nextLine();
-      
-      //takes input for the provider name 
-      System.out.print("Please enter the Provider Name: ");
-      String provider = input.nextLine();
-      
-      //takes the policy holder's First name 
-      System.out.print("Please enter the Policyholder's First Name: ");
-      String nameF = input.nextLine();
-      
-      //takes the policy holder's Last name
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      String  nameL = input.nextLine();
-      
-      //takes policy holder's age
-      System.out.print("Please enter the Policyholder's Age: ");
-      int age = input.nextInt();
-      
-      while (age <=0)// prevents user from enter incorrect data 
-      {
-         System.out.println("Please enter a valid number");
-         System.out.print("Please enter the Policyholder's Age: "); 
-         age = input.nextInt();
-      }
-      
-      //takes policy holders smoking status 
-      System.out.println("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-      String smoke = input.nextLine();
-      
-      input.nextLine();
-      
-      // takes the policy holders height 
-      System.out.println("Please enter the Policyholder's Height (in inches): ");
-      double height = input.nextDouble();
-      
-      while (height < 0)// prevents the user from entering a number below zero 
-      {
-         System.out.println("Please enter a valid number");
-         System.out.print("Please enter the Policyholder's Height (in inches): "); 
-         height = input.nextDouble();
-      }
 
-      // takes the policy holder weight 
-      System.out.println("Please enter the Policyholder's Weight (in pounds): ");
-      double weight = input.nextDouble(); 
+      int number; //user number 
+      String provider; //user provider 
+      String fName;//user first name 
+      String lName;//user last name 
+      int age;//user age 
+      String smoke; //smoker status
+      double height; //user height 
+      double weight; //users weight
+      int smokers = 0; 
       
-      while (weight <0)// prevent the user from entering in a number below zero
+      //array list ot store array object 
+      ArrayList<Policy> policyList = new ArrayList<Policy>();
+      
+      
+      
+      // crate and open the file 
+      File file = new File("PolicyInformation.txt");
+      Scanner inputFile = new Scanner(file);
+      
+      while(inputFile.hasNext())
       {
-         System.out.println("Please enter a valid number");
-         System.out.print("Please enter the Policyholder's Weight (in pounds): "); 
-         weight = input.nextDouble();
+      
+      
+      number = inputFile.nextInt(); 
+      inputFile.nextLine();     
+        
+      provider = inputFile.nextLine(); 
+      
+      fName = inputFile.nextLine();
+      
+      lName = inputFile.nextLine();
+     
+      age = inputFile.nextInt();
+      inputFile.nextLine();
+      
+      smoke = inputFile.nextLine();
+      
+      height = inputFile.nextDouble();
+      
+      weight = inputFile.nextDouble();
+      
+       //make sure we haven't hit the end of the file before trying to skip the blank line
+         if(inputFile.hasNext())
+            inputFile.nextLine();
+         if(inputFile.hasNext())
+            inputFile.nextLine();
+      
+      policyList.add(new Policy( number,  provider,  fName,  lName,  age, smoke, height, weight));
+      
       }
+      
+      /**
+      for (Policy policy : policyList)
+      {
+       System.out.println("Policy Number: "+ policy.getNumber());
+       System.out.println("Provider Name: " + policy.getProvider());
+       System.out.println("Policyholder’s First Name: "+ policy.getFirst());
+       System.out.println("Policyholder’s Last Name: " + policy.getLast());
+       System.out.println("Policyholder’s Age: " + policy.getAge());
+       System.out.println("Policyholder’s Smoking Status: " + policy.getSmokeStatus());
+       System.out.println("Policyholder’s Height: " + policy.getHeight());
+       System.out.println("Policyholder’s Weight: "+ policy.getWeight());
+       System.out.printf("Policyholder’s BMI: %.2f" , policy.getBMI());
+       System.out.printf("Policy Price: $%.2f\n", policy.getPrice());
+       System.out.println();
+       
+       }*/
+       
+       System.out.print(policy);
+       if(policy.getSmokeStatus().equalsIgnoreCase("smoker"))//keep track of the number of smokers
+             smokers ++;
 
       
-      Policy myPolicy = new Policy(number,provider,nameF,nameL,age, smoke,height,weight);// created an instance of the policy class 
+
+       //print out the number of smokers and non-smokers
+      System.out.println("The number of policies with a smoker is: " + smokers);
+      System.out.println("The number of policies with a non-smoker is: " + (policyList.size() - smokers) );
       
-      
-      System.out.println("Policy Number: " + myPolicy.getNumber());//calls the policy class and prints policy number 
-      System.out.println();
-      
-      System.out.println("Provider Name: " + myPolicy.getProvider());//calls the policy class and prints the provider
-      System.out.println();
-      
-      System.out.println("Policyholder's First Name: " + myPolicy.getNameF());//calls the policy class and prints the first name 
-      System.out.println();
-      
-      System.out.println("Policyholder's Last Name: " + myPolicy.getNameL());//calls the policy class and prints the last name 
-      System.out.println();
-      
-      System.out.println("Policyholder's Smoking Status: " + myPolicy.getSmoke());//calls the policy class and prints the last name 
-      System.out.println();
-      
-      System.out.println("Policyholder's Age:" + myPolicy.getAge());//calls the policy class and prints the age 
-      System.out.println();
-      
-      System.out.println("Policyholder's Height: " + myPolicy.getHeight()+ " (inches)");//calls the policy class and prints the height
-      System.out.println();
-      
-      System.out.println("Policyholder's Weight: " + myPolicy.getWeight()+" (lbs)");//calls the policy class and prints the weight 
-      System.out.println();
-      
-      System.out.printf("Policyholder's BMI: %.2f\n ", myPolicy.getBMI());//calls the policy class and prints the BMI method 
-      System.out.println();
-      
-      System.out.printf("Policy Price: $%.2f\n", myPolicy.getPolicyPrice());//calls the policy class and prints the policy price 
    }
 }
